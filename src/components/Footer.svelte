696ae2490c7495c69cab1abdcc9727803264ad3b
Hector Martin <hectormartindama@gmail.com>
2024-07-04 23:27:23 +0200

    feat: add new component

---
+++ src/components/Footer.svelte
@@ -0,0 +1,9 @@
+<footer class="flex items-center justify-center w-full h-[45px] mt-4 border text-black">
+	<p>
+		Made with ❤️ by <a
+			href="https://github.com/HectorMartinDama"
+			target="_blank"
+			class="transition duration-150 hover:underline">HectorMartinDama</a
+		>
+	</p>
+</footer>
