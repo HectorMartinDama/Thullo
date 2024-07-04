1e36e1a1d17bea1e3608ee6fe72d60903d8a6650
Hector Martin <hectormartindama@gmail.com>
2024-07-04 23:26:01 +0200

    feat: new component

---
+++ src/components/Banner.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	export let message: string;
+</script>
+
+<div
+	class="flex items-center justify-center font-semibold text-sm text-white h-14 md:h-10 w-screen bg-gradient-to-r to-[#7877c6] via-[#1080CF] from-[#afabff]"
+>
+	{message}
+</div>
